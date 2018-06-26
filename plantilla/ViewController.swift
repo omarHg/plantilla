c7a1a20651362ecb4df4cf14a1df3e8031f5e6b6
Omar Hernandez Gonzalez <omaglz.92@gmail.com>
2018-06-26 13:07:37 -0500

    Initial Commit

---
+++ plantilla/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  plantilla
+//
+//  Created by Omar Hernandez on 26/06/18.
+//  Copyright © 2018 Omar Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
